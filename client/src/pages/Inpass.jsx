5b37deebabed36ab5a0024da97d028df7d147689
Hariharan.s <hreehs1428@gmail.com>
2025-12-01 10:58:27 +0530

    Update Inpass.jsx

--- client/src/pages/Inpass.jsx
+++ client/src/pages/Inpass.jsx
@@ -1,177 +1,211 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
-import { useFormik } from "formik";
-import { FaRegPaperPlane } from "react-icons/fa";
-import api from "../API/axios";
 import { toast } from "react-toastify";
-import { validationSchema } from "../schema/validationSchema";
-import { Link, useNavigate } from "react-router-dom";
-
-const Register = () => {
-
-  const navigate = useNavigate();
-
-  const onSubmit = async (value, action) => {
-    try {
-      const res = await api.post("/user/register", value);
-
-      toast.success(res.data.message, {
-        position: "top-center",
-        autoClose: 3000,
-        style: {
-          backgroundColor: "black",
-          color: "white",
-          borderRadius: "10px",
-        },
-      });
-
-      action.resetForm();
-      navigate("/login");
-    } catch (error) {
-      toast.error(error.response?.data?.message || "Something went wrong", {
-        position: "top-center",
-        autoClose: 3000,
-        style: {
-          backgroundColor: "black",
-          color: "white",
-          borderRadius: "10px",
-        },
-      });
-    }
-    action.setSubmitting(false);
-  };
-
-  const {
-    values,
-    handleBlur,
-    handleChange,
-    handleSubmit,
-    isSubmitting,
-    errors,
-    touched,
-  } = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      password: "",
+import api from "../API/axios";
+import{useFormik} from 'formik'
+import { inpassSchema } from "../schema/validationSchema";
+import {FaPaperPlane} from 'react-icons/fa'
+import { useNavigate } from "react-router-dom";
+ //onsubmit :
+  const onSubmit = (value,action) => {
+    const navigate =useNavigate()
+    const createInfo = async () => {
+      try {
+        const res = await api.post("/gate-pass",{
+          name:value.name,
+          phone:value.phone,
+          visitorAddress:value.address,
+          purpose:value.purpose,
+          vechileNo:value.vechileNo
+        });
+        navigate('/record')
+    
+          toast.success(res.data.message, {
+          position: "top-center",
+          autoClose: 2000,
+          style: {
+            backgroundColor: "black",
+            color: "white",
+            fontSize: "18px",
+            borderRadius: "10px",
+          },
+        });
+        action.resetForm()
+      } catch (error) {
+  toast.error(error.response?.data?.message || "Something went wrong", {
+    position: "top-center",
+    autoClose: 2000,
+    style: {
+      backgroundColor: "black",
+      color: "white",
+      fontSize: "18px",
+      borderRadius: "10px",
     },
-    validationSchema: validationSchema,
-    onSubmit,
   });
 
+      }
+    };
+
+    createInfo();
+  };
+
+
+const Inpass = () => {
+  const {values,handleBlur,handleChange,handleSubmit,errors,touched,isSubmitting} = useFormik({
+    initialValues :{
+    name: "",
+    phone: "",
+    purpose: "",
+    address: "",
+    vechileNo: "",
+    },
+    validationSchema:inpassSchema,
+    onSubmit
+  })
+  const [message, setMessage] = useState("");
+  const option = [
+    "Admission",
+    "Interview",
+    "Project Review",
+    "Meet Staff",
+    "Meet Student",
+    "Event",
+    "others",
+  ];
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 bg-dark">
-      <div
-        className="card shadow-lg p-4"
-        style={{ width: "40%", borderRadius: "12px" }}
-      >
-        <h2
-          className="text-center mb-3"
-          style={{ fontWeight: 800, color: "yellowgreen" }}
-        >
-          Register
-        </h2>
-        <p className="text-center text-secondary mb-4">
-          Create an account to continue
-        </p>
-
-        <form onSubmit={handleSubmit}>
-
-          <div className="mb-3">
-            <label htmlFor="name" style={{ fontWeight: 700 }}>
-              Full Name
-            </label>
-            <input
-              className={`form-control ${
-                errors.name && touched.name ? "is-invalid" : ""
-              }`}
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Enter your name"
-              value={values.name}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            {touched.name && errors.name && (
-              <p className="invalid-feedback">{errors.name}</p>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="email" style={{ fontWeight: 700 }}>
-              Email Address
-            </label>
-            <input
-              className={`form-control ${
-                errors.email && touched.email ? "is-invalid" : ""
-              }`}
-              type="email"
-              id="email"
-              name="email"
-              placeholder="example@gmail.com"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            {touched.email && errors.email && (
-              <p className="invalid-feedback">{errors.email}</p>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="password" style={{ fontWeight: 700 }}>
-              Password
-            </label>
-            <input
-              className={`form-control ${
-                errors.password && touched.password ? "is-invalid" : ""
-              }`}
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              value={values.password}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            {touched.password && errors.password && (
-              <p className="invalid-feedback">{errors.password}</p>
-            )}
-          </div>
-
-          <button
-            type="submit"
-            className="btn w-100 mt-2 text-white"
-            style={{
-              backgroundColor: "yellowgreen",
-              fontWeight: "700",
-              padding: "10px",
-            }}
-            disabled={isSubmitting}
+    <div className="body bg-dark">
+      <h3 className="text-center text-light " style={{ fontWeight: "800" }}>
+        <span style={{ color: "yellowgreen" }}>In-</span>pass
+      </h3>
+      <p className="text-center text-light">Track visitors Entry by In-pass</p>
+      <form onSubmit={handleSubmit} className="form bg-light">
+        <div className="form-group">
+          <label
+            style={{ fontWeight: "700", margin: "9px" }}
+            className="form-label"
+            htmlFor="name"
           >
-            {isSubmitting ? (
-              <>
-                <FaRegPaperPlane /> Submitting...
-              </>
-            ) : (
-              "Register"
-            )}
-          </button>
+            Enter Visitor's Name
+          </label>
+          <input
+            placeholder="Surya"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={`form-control ${errors.name && touched.name ? 'is-invalid':''}`}
+            type="text"
+            name="name"
+            id="name"
+            value={values.name}
+          />
+          {errors.name && touched.name && <p className="invalid-feedback">{errors.name}</p>}
+        </div>
+        <div className="form-group">
+          <label
+            style={{ fontWeight: "700", margin: "5px" }}
+            className="form-label"
+            htmlFor="phone"
+          >
+            Enter Visitor's Phone
+          </label>
+          <input
+            placeholder="+91 0967542109"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={`form-control ${errors.phone && touched.phone ? 'is-invalid':''}`}
+            type="phone"
+            name="phone"
+            id="phone"
+            value={values.phone}
+          />
+        {errors.phone && touched.phone && <p className="invalid-feedback">{errors.phone}</p>}
 
-          <p className="text-center mt-3" style={{ fontWeight: "600" }}>
-            Already have an account?{" "}
-            <Link
-              to="/login"
-              style={{ color: "yellowgreen", textDecoration: "none" }}
-            >
-              Login
-            </Link>
-          </p>
-        </form>
-      </div>
+        </div>
+        <div className="form-group">
+          <label
+            style={{ fontWeight: "700", margin: "5px" }}
+            className="form-label"
+            htmlFor="address"
+          >
+            Enter Visitor's Address
+          </label>
+          <textarea
+            placeholder="Chennai-60028,Tamilnadu"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={`form-control ${errors.address && touched.address ? 'is-invalid':''}`}
+            type="text"
+            name="address"
+            id="address"
+            value={values.address}
+          />
+        {errors.addredd && touched.address && <p className="invalid-feedback">{errors.addredd}</p>}
+
+        </div>
+        <div className="form-group">
+          <label
+            style={{ fontWeight: "700", margin: "5px" }}
+            className="form-label"
+            htmlFor="name"
+          >
+            Enter Visiting Purpose
+          </label>
+          <select
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={`form-select ${errors.purpose && touched.purpose ? 'is-invalid' : ''}`}
+            type="text"
+            name="purpose"
+            id="name"
+            value={values.purpose}
+          >
+            <option value="">Select Purpose</option>
+            {option.map((data, index) => (
+                <option key={index} value={data}>
+                  {data}
+                </option>
+            ))}
+          </select>
+        {errors.purpose && touched.purpose && <p className="invalid-feedback">{errors.purpose}</p>}
+
+        </div>
+        <div className="form-group">
+          <label
+            style={{ fontWeight: "700", margin: "5px" }}
+            className="form-label"
+            htmlFor="vechileNo"
+          >
+            Enter Visitor's Vechile Number
+          </label>
+          <input
+            placeholder="TN38 BB1234"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className={`form-control ${errors.vechileNo && touched.vechileNo ? 'is-invalid':''}`}
+            type="text"
+            name="vechileNo"
+            id="vechileNo"
+            value={values.vechileNo}
+          />
+        {errors.vechileNo && touched.vechileNo && <p className="invalid-feedback">{errors.vechileNo}</p>}
+
+        </div>
+        <div className="text-center mt-2">
+          <button
+            className="btn"
+            type="submit"
+            style={{
+              background: "yellowgreen",
+              color:'black',
+              width: "100%",
+              fontWeight: "500",
+            }}
+          >
+            {isSubmitting ?(<><FaPaperPlane/> Submiting...</>) : (<>Submit</>)}
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
 
-export default Register;
+export default Inpass;
